f33105206e52f4c9e3214e469cded5b72f475a62
jain-t <tapanupt@gmail.com>
2025-01-02 23:48:22 +0530

    update

--- syncmaster_commons/__init__.py
+++ syncmaster_commons/__init__.py
@@ -2,12 +2,13 @@ __version__ = "0.0.29"
 
 from .abstract import SMBaseClass
 from .agents import AgentRequestPayload, AgentResponsePayload
-from .gupshup import GupshupIncomingPayLoad, GupshupOutgoingPayload, ImagePayload, TextPayload, VideoPayload, AudioPayload, LocationPayload
+from .gupshup import GupshupIncomingPayLoad, GupshupOutgoingPayload, ImagePayload, TextPayload, VideoPayload, AudioPayload, LocationPayload, AgentResponsePayloadGupshup
 from .keys import KEYS
 from .task_names import TaskNames
 
 __all__ = [
     "AgentRequestPayload",
+    "AgentResponsePayloadGupshup",
     "GupshupIncomingPayLoad",
     "KEYS",
     "SMBaseClass",
